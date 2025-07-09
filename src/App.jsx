c10c01911752f4d31c3dacb16c8fba3e0b368178
Ratu Kila <ratukf@gmail.com>
2025-07-10 00:46:56 +0700

    feat: add new routes to Letters Received and Letters Send in main route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { LoginPage } from './pages/LoginPage';
 import { SignUpPage } from './pages/SignUpPage';
 import { DashboardPage } from './pages/DashboardPage';
 import { CreateLetterPage } from './pages/CreateLetterPage';
+import { LettersReceivedPage } from './pages/LettersReceivedPage';
+import { LettersSentPage } from './pages/LettersSentPage';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/create-letter" element={<CreateLetterPage />} />
+        <Route path="/letters-received" element={<LettersReceivedPage />} />
+        <Route path="/letters-sent" element={<LettersSentPage />} />
       </Routes>
     </Router>
   );
